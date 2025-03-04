f5e8006763fd851fdf05b655afff82135dada0fe
michal <stachnikmichal@gmail.com>
2025-03-04 17:51:19 -0500

    added markup to expenses page

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -1,5 +1,24 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import IncomeExpenseChart from '../components/IncomeExpenseChart';
+
 const ExpensesPage = () => {
-  return <div>expenses</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Your expenses</CardTitle>
+        <CardDescription>at a glance</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <IncomeExpenseChart />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default ExpensesPage;
